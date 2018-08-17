02785b7d9444aedd9ed348c28715aa10d8abdc07
Jack Murning <jackrichardmurning@gmail.com>
2018-08-17 15:10:20 +0100

    rename key description to topic

--- javascript-back-end/mock_lessonData.js
+++ javascript-back-end/mock_lessonData.js
@@ -1,6 +1,6 @@
 const mockLessonData = {
   title: "Pirate Joe's First Loop Journey",
-  description: "Help Pirate Joe join Captain Jack's Pirate Ship, the Swally Galley, and program his first adventure learning about functions and loops.",
+  topic: "Help Pirate Joe join Captain Jack's Pirate Ship, the Swally Galley, and program his first adventure learning about functions and loops.",
   questionId: [0]
 }
 
