d85f0d7f5017c0553fd6de989a94176a0ea2300c
Jack Murning <jackrichardmurning@gmail.com>
2018-08-17 14:40:21 +0100

    add first function with story for mock data file

---
+++ javascript-back-end/mock_questionAnswerData.js
@@ -0,0 +1,13 @@
+{
+  {
+    question: "make a gettings for captain jack by touching the buttons in the right order ",
+
+    answers: [
+      "function captainJackGreeting(name){", 'return "Arrgh " + name + ", come aboard";}', 'captainJackGreeting("Joe")'
+    ],
+
+    story: "Joe has just left Pirate School and is looking for a ship to join to sail the seven sea's looking for booty and treasure. Luckily Joe spotted a ship with a large skull and cross bones and walked to the ship. The ships pirates were running about when the ships robot capatin jack walked to the edge of the ship.</br>Captain Jack lost some of his coding that makes him work while fighting Rumbo the Kracken. Do you think you can write the code to make Captain Jack the best Pirate of Bootayland? Pirate Joe thinks you can, lets begin by getting Captain Jack talking to people again by coding a hello program: ",
+
+    result: "Arrgh Joe, come aboard"
+  },
+}
