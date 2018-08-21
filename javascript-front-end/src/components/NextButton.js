1c665c3408df2d3e01d44bd9c000e0a0ccfd2a8d
Steven Bonner <stevenbonner1990@gmail.com>
2018-08-21 15:08:15 +0100

    added previous button

---
+++ javascript-front-end/src/components/NextButton.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+class NextButton extends React.Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      buttonClicked: true
+    }
+    this.handleNextButton = this.handleNextButton.bind(this)
+      }
+
+
+        handleNextButton(event) {
+        console.log("lknoln");
+
+        }
+    render(){
+      return(
+        <button onClick={this.handleNextButton}> Next Task</button>
+      )
+    }
+      }
+
+export default NextButton;
--- javascript-front-end/src/containers/Question.js
+++ javascript-front-end/src/containers/Question.js
@@ -13,6 +13,7 @@ class Question extends React.Component  {
       questionCompleted: false
     }
     this.handleNext = this.handleNext.bind(this)
+    this.handlePrevious = this.handlePrevious.bind(this)
   }
 
   handleNext(){
@@ -25,6 +26,18 @@ class Question extends React.Component  {
 
     })
   )
+}
+
+handlePrevious(){
+  if(this.state.questionNumber === this.props.lesson.length + 1){
+
+  }
+  this.setState(prevState => ({
+    questionNumber: prevState.questionNumber - 1,
+    questionCompleted: false
+
+  })
+)
 }
 
   render(){
@@ -41,6 +54,7 @@ class Question extends React.Component  {
       </div>
        <FunctionContainer functionLines={this.props.lesson[this.state.questionNumber].answers} />
        <button onClick={this.handleNext}>Next</button>
+       <button onClick={this.handlePrevious}>Previous</button>
 
      </React.Fragment>
     );
